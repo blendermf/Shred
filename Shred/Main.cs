60758881ba1db94859c2255de721533e6b60fe56
Matthew Fraser <1923697+blendermf@users.noreply.github.com>
2019-09-30 12:36:42 -0400

    Initial Mod Structure (does nothing).

---
+++ Shred/Main.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using Harmony12;
+using System.Reflection;
+using UnityModManagerNet;
+using System;
+
+namespace Shred {
+
+    [Serializable]
+    public class Settings : UnityModManager.ModSettings {
+
+        public Settings() : base() {
+
+        }
+
+        public override void Save(UnityModManager.ModEntry modEntry) {
+
+            UnityModManager.ModSettings.Save<Settings>(this, modEntry);
+
+        }
+    }
+
+    static class Main {
+
+        public static bool enabled = false;
+        public static Settings settings;
+        public static String modId;
+        public static HarmonyInstance harmonyInstance;
+
+        static bool Load(UnityModManager.ModEntry modEntry) {
+
+            settings = Settings.Load<Settings>(modEntry);
+            modId = modEntry.Info.Id;
+            modEntry.OnToggle = OnToggle;
+
+            return true;
+
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) {
+
+            if (enabled == value) return true;
+            enabled = value;
+
+            if (enabled) {
+                // Do patching on enable
+                harmonyInstance = HarmonyInstance.Create(modEntry.Info.Id);
+                harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            } else {
+                // Remove patches when disabled
+                harmonyInstance.UnpatchAll(harmonyInstance.Id);
+            }
+
+            return true;
+
+        }
+    }
+
+}
