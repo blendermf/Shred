887ea9f22661e90259edeb456adf569d46f6562e
Matthew Fraser <1923697+blendermf@users.noreply.github.com>
2019-09-30 13:56:08 -0400

    Added code to allow for custom mod game states (like the ones for the different menus, gear selector, replay editor, etc.)

--- Shred/Lib/StateManager.cs
+++ Shred/Lib/StateManager.cs
@@ -2,8 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using GameManagement;
 
 namespace Shred.Lib {
-    class StateManager {
+    class StateManager : MonoBehaviour {
+        private Dictionary<Type, HashSet<Type>> allowedTransitions = new Dictionary<Type, HashSet<Type>>();
+        private static object padlock = new object();
+
+        public static StateManager Instance { get; private set; }
+
+        private void Awake() {
+            lock (padlock) {
+                if (StateManager.Instance != null) {
+                    Debug.LogError("More than one StateManager");
+                } else {
+                    StateManager.Instance = this;
+                }
+            }
+        }
+
+        public void SetAllowedTransition(Type fromState, Type toState) {
+            if (!allowedTransitions.ContainsKey(fromState)) {
+                allowedTransitions[fromState] = new HashSet<Type>();
+            }
+
+            allowedTransitions[fromState].Add(toState);
+        }
+
+        public void RemoveAllowedTransition(Type fromState, Type toState) {
+            if (allowedTransitions.ContainsKey(fromState)) {
+                allowedTransitions[fromState].Remove(toState);
+
+                if (!allowedTransitions[fromState].Any()) {
+                    allowedTransitions.Remove(fromState);
+                }
+            }
+        }
+
+        public bool CanDoTransition(Type fromState, Type toState) {
+            return allowedTransitions.ContainsKey(fromState) && allowedTransitions[fromState].Contains(toState);
+        }
     }
 }
---
+++ Shred/Patches/GameStateMachinePatches.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Harmony12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameManagement;
+using System.Reflection;
+using Shred.Mod;
+using Shred.Lib;
+
+namespace Shred.Patches {
+    [HarmonyPatch(typeof(GameStateMachine), "Update")]
+    static class GameStateMachine_Update_Patch {
+  
+        static bool Prefix(GameStateMachine __instance) {
+            if (Main.enabled) {
+                //__instance.CurrentState.OnUpdate();
+                return true;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GameState), "CanDoTransitionTo")]
+    static class PauseState_CanDoTransitionTo_Patch {
+        private static string NullableToString(object obj) {
+            return obj == null ? "(null)" : obj.ToString();
+        }
+        static bool Prefix(GameState __instance, Type targetState, ref bool __result, Type[] ___availableTransitions) {
+            if (Main.enabled) {
+                __result = ___availableTransitions.Contains(targetState) || StateManager.Instance.CanDoTransition(__instance.GetType(), targetState);
+
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
